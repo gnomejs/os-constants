5bc73b86091fc0caae8c72f3242cc93493e6a5a2
BowTiedGnome <119234633+bowtiedgnome@users.noreply.github.com>
2024-04-29 22:27:34 -0500

    add module doc

--- mod.ts
+++ mod.ts
@@ -1,3 +1,23 @@
+/**
+ * os-constants provides constant values for basic os information
+ * such as `PLATFORM`, `ARCH`, `WINDOWS`, `DARWIN`, `PATH_SEP`, `DEV_NULL`
+ * 
+ * ## Basic Usage
+ * 
+ * ```typescript
+ * import * as os from "@gnome/os-constants";
+ * 
+ * console.log(os.PLATFORM);
+ * console.log(os.ARCH);
+ * console.log("windows", os.WINDOWS);
+ * console.log("darwin", os.DARWIN);
+ * console.log("linux", os.LINUX);
+ * console.log("eol", os.EOL);
+ * console.log("path separator", os.PATH_SEP);
+ * ```
+ * @module
+ */
+
 import { RUNTIME } from "@gnome/runtime-info";
 
 export type OsPlatform =
